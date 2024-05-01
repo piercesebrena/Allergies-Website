34250e95a3647f0c08805087adc99fbc64ecf286
piercesebrena <146661390+piercesebrena@users.noreply.github.com>
2024-04-30 18:06:47 -0700

    changes

---
+++ app.js
@@ -0,0 +1,33 @@
+// require("dotenv").config(); //add from MongoDB doc
+// require("./config/connection"); //add from MongoDB doc
+const express = require("express");
+const morgan = require("morgan");
+const path = require("path");
+const app = express();
+const PORT = 3000;
+const cors = require("cors");
+
+app.use(morgan("dev"));
+
+// const booksRoutes = require('./routes/booksRouter');
+// const authorsRoutes = require('./routes/authorsRouter');
+// const adminRoutes = require('./routes/adminRouter'); 
+// const siteRoutes = require('./routes/siteRouter'); //Define the routing variable for siteRoutes
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname + "/public")));
+
+app.get("/", (request, response, next) => {
+  response.status(200).json({success: {message: "This route points to the Home page"}, statusCode: 200});
+});
+
+// app.use(booksRoutes);
+// app.use(authorsRoutes);
+// app.use(adminRoutes); 
+// app.use(siteRoutes); //Tell the app to use the routing variables you defined earlier, siteRoutes 
+
+app.listen(PORT, () => {
+  console.log(`Personal project server is listening on port ${PORT}`);
+  console.log(`http://localhost:${PORT}/`)
+});
